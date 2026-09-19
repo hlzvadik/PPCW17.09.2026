e632e95bccd6bff2d1994a5b96e876d72098f718
hlzvadik <golcovv716@gmail.com>
2026-09-19 17:36:14 +0300

    Add isInside and calc

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <random>
+
+size_t calc(double r, size_t tests, size_t seed) {
+  std::default_random_engine eng(seed);
+  std::uniform_real_distribution< double > dist(0, 2 * r);
+  size_t res = 0;
+  for (size_t i = 0; i < tests; ++i) {
+    res += isInside(dist(eng), dist(eng), r) ? 1 : 0;
+  }
+  return res;
+}
+
+bool isInside(double x, double y, double r) {
+  return ((r - x) * (r - x)) <= r && ((r - y) * (r - y)) <= r;
+}
 
 int main() {
 
